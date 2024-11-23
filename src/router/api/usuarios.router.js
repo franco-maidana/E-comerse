592c54949de0ad3e23f5cdb969abaed596d0e8d7
Franco Maidana <francomaidana094@gmail.com>
2024-11-23 00:05:42 -0300

    se modifico el usuario.router.js para prueba

--- src/router/api/usuarios.router.js
+++ src/router/api/usuarios.router.js
@@ -3,16 +3,6 @@ import Conexion from "../../utils/ConexionMySQL.js"; // Asegúrate de que este a
 
 const usuariosRouter = Router();
 
-usuariosRouter.get('/prueba', async (req, res, next) => {
-  try {
-    const [usersMySQL] = await Conexion.query('SELECT * FROM usuarios');  // Agrega await aquí
-    res.status(200).json({
-      message: 'Usuarios de la base de datos',
-      usuarios: usersMySQL  // Ahora usersMySQL es un array
-    });
-  } catch (error) {
-    return next(error);  // Manejo de errores
-  }
-});
+
 
 export default usuariosRouter;
